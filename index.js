20a927ddec7811316af0ea2ff3379b316c3d7d58
Antoine <36891353+aloizel@users.noreply.github.com>
2026-03-23 16:28:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
  
 // Change cette URL par ton vrai lien PDF
-const PDF_URL = "https://ton-serveur.com/monproduit.pdf";
+const PDF_URL = "https://img.freepik.com/vecteurs-libre/vecteur-conception-degrade-colore-oiseau_343694-2506.jpg?semt=ais_hybrid&w=740&q=80";
  
 app.get("/pdf", (req, res) => {
   res.json({

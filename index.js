f59e757f4bf86c6f69868b51bff318fa922a831c
Antoine <36891353+aloizel@users.noreply.github.com>
2026-03-24 15:04:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,24 +6,42 @@ const PORT = process.env.PORT || 3000;
 const PDF_URL = "https://www.orimi.com/pdf-test.pdf"; //"https://i.imgur.com/aYqhIzz.jpeg";
  
 app.get("/pdf", (req, res) => {
-  res.json({
-    messages: [
-      {
-        type: "ATTACHMENT", // "GALLERY",
-        messageNumber: 1,
-        attachment: {
-         type: "file",
-         url: PDF_URL,
-        },
-       // elements: [
-       //   {
-       //     title: "Mon image",
-       //     imageUrl: PDF_URL
-       //   }
-       // ]
-      },
-    ],
-  });
+  // res.json({
+  //   messages: [
+  //     {
+  //       type: "ATTACHMENT", // "GALLERY",
+  //       messageNumber: 1,
+  //       attachment: {
+  //        type: "file",
+  //        url: PDF_URL,
+  //       },
+  //      // elements: [
+  //      //   {
+  //      //     title: "Mon image",
+  //      //     imageUrl: PDF_URL
+  //      //   }
+  //      // ]
+  //     },
+  //   ],
+  // });
+ res.json({
+  messages: [
+    {
+      type: "DEFAULT",
+      messageNumber: 1,
+      text: "Voici votre document :",
+      buttons: [
+        {
+          type: "web_url",
+          url: "https://www.orimi.com/pdf-test.pdf",
+          title: "Ouvrir le PDF",
+          buttonNumber: 1,
+          messengerExtensions: false
+        }
+      ]
+    }
+  ]
+});
 });
  
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
